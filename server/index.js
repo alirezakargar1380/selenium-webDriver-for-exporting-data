06f41a2bb329ec76f0fca9a51733d1f96516df93
alireza kargar <imanesche@gmail.com>
2021-09-07 17:58:35 +0430

    add packaje files

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const app = require("./app");
+app.listen(3001, () => {
+    console.log("you can see your code here: http://localhost:3001/result")
+})
+
+// const combinations = require('combinations');
+// var myArray = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ];
+//
+// combinations(myArray, 11,11)
+// console.log(combinations(myArray, 11,11))
+
+
+// const combinate = require('combinate');
+// import combinate from "combinate";
+//
+//
+// const values = {
+//   color: ["red", "blue", "green"],
+//   mode: ["light", "dark"],
+// };
+//
+// const combinations = combinate(values);
+//
+// console.log(combinations);
+
+
