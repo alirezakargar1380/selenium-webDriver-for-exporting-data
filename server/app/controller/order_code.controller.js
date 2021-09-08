48dc744bad7d9744963c239e554f92bdbb8dc920
alireza kargar <imanesche@gmail.com>
2021-09-08 20:30:35 +0430

    order code set limit for api

--- server/app/controller/order_code.controller.js
+++ server/app/controller/order_code.controller.js
@@ -1,10 +1,21 @@
 const response = require("../utils/response.utitlity")
+const order_code_Service = require('../service/order_code');
 
-exports.select_all = (req, res) => {
+exports.select_all = async (req, res) => {
   try {
-
+    const { offset, limit } = req.params
+    const result = await order_code_Service.get(parseInt(offset), parseInt(limit))
+    response.success(res, result)
   } catch (e) {
-    console.log('controller');
-    response.exception(res, e.message)
+    response.exception(res, e)
+  }
+}
+
+exports.delete_all = async (req, res) => {
+  try {
+    const result = await order_code_Service.TRUNCATE()
+    response.success(res, result)
+  } catch (e) {
+    response.exception(res, e)
   }
 }
--- server/app/routers/order_code.router.js
+++ server/app/routers/order_code.router.js
@@ -2,7 +2,12 @@ const express = require("express");
 const Router = express.Router();
 const controller = require("./../controller/order_code.controller")
 
-Router.route('/order_code/get/:offset/:limit')
+Router
+    .route('/order_code/get/:offset/:limit')
     .get(controller.select_all)
+
+Router
+    .route('/order_code/delete_all')
+    .delete(controller.delete_all)
 
 module.exports = Router;
