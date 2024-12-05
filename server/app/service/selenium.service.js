cf4d66ff660fafd5d3f6b99dd336e6c2514469bd
alireza kargar <alirezakargarsuport@gmail.com>
2024-12-05 10:03:14 -0800

    add headless selenium

--- server/app/service/selenium.service.js
+++ server/app/service/selenium.service.js
@@ -1,6 +1,10 @@
-const {Builder, By, Key, util} = require("selenium-webdriver");
+const { Builder, By, Key, util } = require("selenium-webdriver");
 const Exception = require("../utils/error.utility");
 const log = require("../utils/log.utility");
+
+const chrome = require('selenium-webdriver/chrome');
+const options = new chrome.Options();
+options.addArguments('--headless');
 
 // v-Notification system v-Notification-system v-position-top v-position-center
 module.exports = class SeleniumService {
@@ -13,7 +17,10 @@ module.exports = class SeleniumService {
   }
 
   async open_browser() {
-    this.driver = await new Builder().forBrowser("firefox").build()
+    this.driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build()
   }
 
   quit() {
@@ -21,32 +28,25 @@ module.exports = class SeleniumService {
   }
 
   async open_webpage() {
-    try {
-      await this.driver.get(this.url)
-          .then(() => {
-            this.result.status = true
-          })
-          // .catch(() => {
-          //   console.log('catch in driver.get ')
-          //   this.driver.quit()
-          //   this.result.status = false
-          // })
-      if (this.result.status) return this.result
-      // return this.result
-    } catch (e) {
-      // console.log('quit from selenium...')
-      // this.result.status = false
-      await this.driver.quit()
-      throw Exception.setError(e)
-    }
+    // try {
+    await this.driver.get(this.url)
+      .then(() => {
+        this.result.status = true
+      })
+      .catch((e) => console.log(e))
+    if (this.result.status) return this.result
+    // } catch (e) {
+    //   await this.driver.quit()
+    //   throw Exception.setError(e)
+    // }
   }
 
   async add_execute_script() {
     try {
       await this.driver
-          .executeScript(`document.querySelectorAll('.v-textfield-search-input')[0].setAttribute("name","qqq");`);
+        .executeScript(`document.querySelectorAll('.v-textfield-search-input')[0].setAttribute("name","qqq");`);
       await this.driver
-          .executeScript("document.querySelectorAll('.v-button-search-button')[0].setAttribute('name','btntnntn');");
+        .executeScript("document.querySelectorAll('.v-button-search-button')[0].setAttribute('name','btntnntn');");
     } catch (e) {
       this.quit()
       throw Exception.setError(e)
@@ -56,40 +56,38 @@ module.exports = class SeleniumService {
   async send_order_code_for_search(value) {
     try {
       await this.driver
-          .findElement(By.name('qqq'))
-          .sendKeys(value)
-          .then(() => {
-            this.result.status = true
-          })
+        .findElement(By.name('qqq'))
+        .sendKeys(value)
+        .then(() => {
+          this.result.status = true
+        })
     } catch (e) {
       // this.quit()
       throw Exception.setError(e)
     }
   }
 
-  async click_on_search_btn()
-  {
+  async click_on_search_btn() {
     try {
-    await this.driver
+      await this.driver
         .findElement(By.name('btntnntn'))
         .click()
-    //
-    // await this.driver
-    //     .wait(this.driver.executeScript(() => {
-    //         return document.readyState
-    //     })).then(() => {
-    //         console.log('code loaded...')
-    //     }).catch((e) => {
-    //         console.log(e)
-    //     })
+      //
+      // await this.driver
+      //     .wait(this.driver.executeScript(() => {
+      //         return document.readyState
+      //     })).then(() => {
+      //         console.log('code loaded...')
+      //     }).catch((e) => {
+      //         console.log(e)
+      //     })
     } catch (e) {
       // this.quit()
       throw Exception.setError(e)
     }
   }
 
-  async none_loading()
-  {
+  async none_loading() {
     try {
       await this.driver.executeScript(() => {
         return document.querySelectorAll('.v-loading-indicator')[0].style.display = "none"
@@ -105,7 +103,7 @@ module.exports = class SeleniumService {
     try {
       await this.driver.executeScript(() => {
         return document.querySelectorAll('.v-loading-indicator')[0].style.display
-      }).then( async (res) => {
+      }).then(async (res) => {
         if (res !== "none") await this.checking_loading()
       })
     } catch (e) {
@@ -117,9 +115,8 @@ module.exports = class SeleniumService {
     try {
       return await this.driver.executeScript(() => {
         return document.querySelectorAll('.v-Notification-system')[0]
-      }).then( (res) => {
-        if (res !== null)
-        {
+      }).then((res) => {
+        if (res !== null) {
           log.error("------------------------------------------------------------------------- session was expire")
           return false
         }
@@ -130,8 +127,7 @@ module.exports = class SeleniumService {
     }
   }
 
-  async check_for_extend_value()
-  {
+  async check_for_extend_value() {
     try {
       await this.driver.executeScript(() => {
         return document.querySelectorAll('.v-window-wrap')[0].setAttribute('id', 'errorrrplace');
@@ -154,9 +150,9 @@ module.exports = class SeleniumService {
   async error_box_execute_script() {
     try {
       await this.driver
-          .executeScript(() => {
-            return document.querySelectorAll('div.v-button.v-widget')[3].setAttribute('id', 'error-box-ok-btn');
-          })
+        .executeScript(() => {
+          return document.querySelectorAll('div.v-button.v-widget')[3].setAttribute('id', 'error-box-ok-btn');
+        })
     } catch (e) {
       // this.quit()
       throw Exception.setError(e)
