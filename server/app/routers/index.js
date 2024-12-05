63bd550a032922b41772d1889ebcb15785d69e4b
alireza kargar <alirezakargarsuport@gmail.com>
2024-12-05 10:02:58 -0800

    Update index.js

--- server/app/routers/index.js
+++ server/app/routers/index.js
@@ -2,9 +2,6 @@
 module.exports = (app) => {
   app.use("/api",
       require("./file"),
-      // require("./genrator"),
-      // require("./handeling"),
-      // require("./selenium"),
       require("./file.router"),
       require("./selenium.router"),
       require("./order_code.router"),
